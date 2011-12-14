f853f9004ecb2a9cbbb8bc39f4226663b021a27a
Jone Eide <jone@idev.no>
2011-12-15 00:24:03 +0100

    Added Course API urls

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import patterns, url, include
 # URL includes for API
 urlpatterns = patterns('',
     url(r'', include('players.urls_api')),
+    url(r'', include('courses.urls_api')),
     url(r'', include('games.urls_api')),
 )
