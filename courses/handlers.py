365c492462972965dd46ae115a19547da1d5abc9
Jone Eide <jone@idev.no>
2012-01-10 22:39:15 +0100

    Improved Course Piston handler

--- courses/handlers.py
+++ courses/handlers.py
@@ -9,15 +9,35 @@ from courses.models import Course, CourseHole
 class CourseHandler(BaseHandler):
     allowed_methods = ("GET",)
     model = Course
-    fields = ("id", "name",)
+    fields = ("id", "name", )
 
     def read(self, req, pk=None):
         base = Course.objects
 
         if pk is not None:
-            return base.get(pk=pk)
+            return self._get_course(base.get(pk=pk))
 
-        return base.all()
+        return [self._get_course(c) for c in base.all()]
+
+    def _get_course(self, course):
+        return {
+            'id': course.id,
+            'name': course.name,
+            'courseholes': self._get_courseholes(course),
+        }
+
+    def _get_courseholes(self, course):
+        courseholes = []
+
+        for coursehole in course.courseholes.all():
+            courseholes.append({
+                'id': coursehole.id,
+                'par': coursehole.hole.par,
+                'order': coursehole.order,
+                'name': coursehole.name,
+            })
+
+        return courseholes
 
 
 class CourseHoleHandler(BaseHandler):
